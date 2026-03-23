9a07e6b07a10107e4cc6f03b78b9f51b715f75a4
Do Manh Tung <51911941+domanhtung@users.noreply.github.com>
2026-03-23 17:19:29 +0700

    fix: bug unload image when build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
